53b2b370c944d986fd0993f2a76ec0261d015b55
JuanFila <juanfilapablo@hotmail.com>
2023-12-21 23:40:38 -0300

    add header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,13 @@
 import { Container } from "./styles.js";
 import { Button } from "../../components/Button/index.jsx";
+import { Header } from "../../components/Header/Header.jsx";
 
 export function Details() {
 
   return (
     <Container>
-      <h1>tese</h1>
-      <span>teste2</span>
-      <Button title="Entrar" loading />
-      <Button title="Voltar"/>
-      <Button title="Sair"/>      
+      <Header/>
+      <Button title="Voltar"/>  
     </Container>
   )
 }
